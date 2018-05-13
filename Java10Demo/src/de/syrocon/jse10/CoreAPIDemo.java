bb0ce512402fd5fa08501846db40b7d5581ad808
Alexander Rühl <alexander.ruehl@syrocon.de>
2018-05-13 08:52:46 +0200

    add local type inference demo

--- Java10Demo/src/de/syrocon/jse10/CoreAPIDemo.java
+++ Java10Demo/src/de/syrocon/jse10/CoreAPIDemo.java
@@ -1,16 +1,40 @@
 package de.syrocon.jse10;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class CoreAPIDemo {
 
 	public static void main(String[] args) {
-		System.out.println("Show unmodifiable list:");
+		System.out.println("Show local type inference:");
+		showLocalTypeInference();
+
+		System.out.println("\nShow unmodifiable list:");
 		showUnmodifiableList();
 
 		System.out.println("\nShow optional throw:");
 		showOptionalThrow();
+	}
+
+	@SuppressWarnings("unused")
+	private static void showLocalTypeInference() {
+		var zahl = 5; // int
+		var flag = true; // boolean
+		var text = "Hello World"; // String
+		System.out.println(text instanceof String);
+		String var;
+		zahl = 1; // zahl = 1.5 results in compile error
+		
+		var numbers = Arrays.asList(1, 2, 3); // List<Integer>
+		for (var number: numbers) { // Integer
+			System.out.println(number instanceof Integer);
+		    System.out.println(number);
+		}
+		
+		var factor = 2; // effectively final
+		numbers = List.of(2, 16, 4, 12, 38, 15, 1);
+		numbers.stream().forEach(v -> System.out.println(v * factor)); // final variable can be used in lambda expression
 	}
 
 	private static void showUnmodifiableList() {
@@ -25,7 +49,8 @@ public class CoreAPIDemo {
 		try {
 			Integer empty = numbers.stream().filter(v -> v < 0).findFirst().orElseThrow(); // throws java.util.NoSuchElementException
 		} catch (java.util.NoSuchElementException e) {
-			System.out.println("No element in filtered stream");
+			System.out.println("Hello World!");
 		}
 	}
+
 }
