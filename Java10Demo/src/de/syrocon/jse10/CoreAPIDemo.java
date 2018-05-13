ef0f81fdd8763b99fc77ad2c63529a3bc80c0c0f
Alexander Rühl <alexander.ruehl@syrocon.de>
2018-05-13 07:07:04 +0200

    add optional demo

--- Java10Demo/src/de/syrocon/jse10/CoreAPIDemo.java
+++ Java10Demo/src/de/syrocon/jse10/CoreAPIDemo.java
@@ -6,9 +6,26 @@ import java.util.stream.Collectors;
 public class CoreAPIDemo {
 
 	public static void main(String[] args) {
+		System.out.println("Show unmodifiable list:");
+		showUnmodifiableList();
+
+		System.out.println("\nShow optional throw:");
+		showOptionalThrow();
+	}
+
+	private static void showUnmodifiableList() {
 		List<Integer> numbers = List.of(2, 16, 4, 12, 38, 15, 1);
 		List<Integer> singleDigits = numbers.stream().filter(v -> v < 10).collect(Collectors.toUnmodifiableList());
 		singleDigits.forEach(System.out::println);
 	}
 
+	@SuppressWarnings("unused")
+	private static void showOptionalThrow() {
+		List<Integer> numbers = List.of(2, 16, 4, 12, 38, 15, 1);
+		try {
+			Integer empty = numbers.stream().filter(v -> v < 0).findFirst().orElseThrow(); // throws java.util.NoSuchElementException
+		} catch (java.util.NoSuchElementException e) {
+			System.out.println("No element in filtered stream");
+		}
+	}
 }
