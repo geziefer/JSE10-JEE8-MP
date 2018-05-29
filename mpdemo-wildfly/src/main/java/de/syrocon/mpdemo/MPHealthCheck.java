2fa2397e31da3d2e3d7c92bdead09efe9af214cb
Alexander Rühl <alexander.ruehl@syrocon.de>
2018-05-29 21:05:20 +0200

    add microprofile demo projects for payara micro and wildfly swarm

---
+++ mpdemo-wildfly/src/main/java/de/syrocon/mpdemo/MPHealthCheck.java
@@ -0,0 +1,17 @@
+package de.syrocon.mpdemo;
+
+
+import javax.enterprise.context.ApplicationScoped;
+
+import org.eclipse.microprofile.health.Health;
+import org.eclipse.microprofile.health.HealthCheck;
+import org.eclipse.microprofile.health.HealthCheckResponse;
+
+@Health
+@ApplicationScoped
+public class MPHealthCheck implements HealthCheck {
+	public HealthCheckResponse call() {
+		return HealthCheckResponse.named("ping").up().withData("AS: OK", "DB: OK").build();
+	}
+
+}
