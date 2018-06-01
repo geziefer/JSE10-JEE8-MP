692f2aee57ecd013edd3f4eab57b9e419b968b9f
Alexander Rühl <alexander.ruehl@syrocon.de>
2018-06-01 13:02:49 +0200

    finisthed jsf demo

--- jee8demo/src/main/java/de/syrocon/jee8demo/jsf/GreetingEndpoint.java
+++ jee8demo/src/main/java/de/syrocon/jee8demo/jsf/GreetingEndpoint.java
@@ -1,15 +1,25 @@
 package de.syrocon.jee8demo.jsf;
 
+import java.time.LocalTime;
+
+import javax.enterprise.context.ApplicationScoped;
 import javax.faces.push.Push;
 import javax.faces.push.PushContext;
 import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
 
+@ApplicationScoped
+@Path("/jsfdemo")
 public class GreetingEndpoint {
 	@Inject
 	@Push(channel = "ticker")
 	private PushContext channel;
 
-	public void notify(String message) {
+	@GET
+	public void push() {
+		String message = "Hello at " + LocalTime.now();
+		System.out.println("Send \"" + message + "\"");
 		this.channel.send(message);
 	}
 }
